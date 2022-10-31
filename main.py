5fdccb851cb96d353ce9141922cdc2ced5331706
SK <sarthakkarandikar03@gmail.com>
2022-10-31 08:21:15 +0000

    Oct 31, 2022, 1:51 PM

--- main.py
+++ main.py
@@ -38,8 +38,4 @@ label = tkinter.Label(screen, text="Get your image watermarked", font=('Montserr
 label.place(x=155, y=200)
 button=tkinter.Button(screen,text="Upload image",command=image_file)
 button.place(x=205,y=250)
-
-
-    
-
 screen.mainloop()
