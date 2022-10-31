e39a4c7262a5d013a2b50ec31392ad5ec96f202c
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:57:26 +0000

    Oct 31, 2022, 10:27 AM

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import tkinter
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 
-screen=tkinter.Tk()
+screen = tkinter.Tk()
 screen.title("Watermark")
 screen.geometry("500x500")
-canvas=tkinter.Canvas(screen,height=100,width=100,bg="black")
-canvas.place(x=200,y=50)
-logo=ImageTk.PhotoImage(Image.open("./logo.jpg"))
-canvas.create_image(10,10,image=logo,anchor=tkinter.NW)
-label=tkinter.Label(screen,text="Upload your image below")
-label.place(x=185,y=200)
+canvas = tkinter.Canvas(screen, height=100, width=100, bg="black")
+canvas.place(x=200, y=50)
+logo = ImageTk.PhotoImage(Image.open("./logo.jpg"))
+canvas.create_image(10, 10, image=logo)
+label = tkinter.Label(screen, text="Upload your image below")
+label.place(x=185, y=200)
 
 screen.mainloop()
