5533fcbe6d099cffc322229cbd5118aec6cffe94
SK <sarthakkarandikar03@gmail.com>
2022-10-31 05:51:51 +0000

    Oct 31, 2022, 11:21 AM

--- utils.py
+++ utils.py
@@ -3,4 +3,5 @@ from tkinter import filedialog
 
 
 def image_file():
-    file=filedialog.askopenfile(mode="rb",filetypes=[('JPG File', '*.jpg'),('PNG File','*.png')])
+    file=filedialog.askopenfile(mode="rb",filetypes=[('JPG File', '*.jpg'),('PNG File','*./png')])
+    
