432b6b3be23bcc15e2bbc598f9711c56996f857b
SK <sarthakkarandikar03@gmail.com>
2022-10-31 05:47:26 +0000

    Oct 31, 2022, 11:17 AM

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ logo=Image.open("./logo.jpg")
 resized_logo=logo.resize((100,100),Image.ANTIALIAS)
 new_logo=ImageTk.PhotoImage(image=resized_logo)
 canvas.create_image(0, 0, image=new_logo, anchor=tkinter.NW)
-label = tkinter.Label(screen, text="")
+label = tkinter.Label(screen, text="Get your image watermarked")
 label.place(x=185, y=200)
+
 
 screen.mainloop()
