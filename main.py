113bf41209e0dd0b7d7eed85bd9bc23928760955
SK <sarthakkarandikar03@gmail.com>
2022-10-31 09:08:10 +0000

    Oct 31, 2022, 2:38 PM

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def image_file():
         stream = BytesIO(img_bytes)
         img = Image.open(stream).convert("RGB")
         stream.close()
-        img.save(fp="user_image.jpg")
+        img.save(fp="userimage.jpg")
         watermark_page()
     else:
         messagebox.showerror('Upload error','No such file found')
@@ -26,7 +26,8 @@ def watermark_page():
     button1.destroy()
     canvas2 = tkinter.Canvas(screen, height=300, width=300, bg="black")
     canvas2.place(x=100, y=50)
-    img=Image.open('./user_image.jpg')
+    img=Image.open("./userimage.jpg")
+    print(img)
     resized_img=img.resize((300,300),Image.ANTIALIAS)
     new_img=ImageTk.PhotoImage(image=resized_img)
     print(new_img)
