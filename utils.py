2267c670ede47e6be4617209e46685b080ca947b
SK <sarthakkarandikar03@gmail.com>
2022-10-31 06:10:46 +0000

    Oct 31, 2022, 11:40 AM

--- utils.py
+++ utils.py
@@ -14,3 +14,4 @@ def watermark_image(img):
     text=Image.new("RGBA",base.size,(255,255,255,0))
     d=ImageDraw.Draw(text)
     d.text((0,0),"SK",font="Arial",fill=(255,255,255,128))
+    out=Image
