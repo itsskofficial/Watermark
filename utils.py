9943737cc3dbd23efc1d82176a2a150364fa6624
SK <sarthakkarandikar03@gmail.com>
2022-10-31 06:15:06 +0000

    Oct 31, 2022, 11:45 AM

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def image_file():
     file=tkinter.filedialog.askopenfile(mode="rb",filetypes=[('JPG File', '*.jpg'),('PNG File','*./png')])
     if file is not None:
         img=file.read()
-        return img
+        
     else:
         tkinter.messagebox.showerror('Upload error','No such file found')
 
