90207e45e3aad6f502402b11150fbe4db4d84ceb
SK <sarthakkarandikar03@gmail.com>
2022-10-31 05:48:27 +0000

    Oct 31, 2022, 11:18 AM

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ new_logo=ImageTk.PhotoImage(image=resized_logo)
 canvas.create_image(0, 0, image=new_logo, anchor=tkinter.NW)
 label = tkinter.Label(screen, text="Get your image watermarked")
 label.place(x=185, y=200)
-button=tkinter.Button(screen,text="Upload image",)
+
+button=tkinter.Button(screen,text="Upload image",command=file())
 
 screen.mainloop()
