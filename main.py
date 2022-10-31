123cdb91e368bbcf23c4e3441fa4a92dc3358cee
SK <sarthakkarandikar03@gmail.com>
2022-10-31 09:04:51 +0000

    Oct 31, 2022, 2:34 PM

--- main.py
+++ main.py
@@ -27,9 +27,10 @@ def watermark_page():
     button1.destroy()
     canvas2 = tkinter.Canvas(screen, height=300, width=300, bg="black")
     canvas2.place(x=100, y=50)
-    # resized_img=img.resize((300,300),Image.ANTIALIAS)
-    # new_img=ImageTk.PhotoImage(image=resized_img)
-    # print(new_img)
+    img=Image.open
+    resized_img=img.resize((300,300),Image.ANTIALIAS)
+    new_img=ImageTk.PhotoImage(image=resized_img)
+    print(new_img)
     canvas2.create_image(0,0,image=img,anchor=tkinter.NW)
 
 screen = tkinter.Tk()
