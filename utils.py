6de82484f7acba899ed9fe4b0bf4efec22451f94
SK <sarthakkarandikar03@gmail.com>
2022-10-31 06:13:31 +0000

    Oct 31, 2022, 11:43 AM

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def image_file():
         img=file.read()
     else:
         tkinter.messagebox.showerror('Upload error','No such file found')
-    
+    watermark
 
 def watermark_image(img):
     base=img.convert("RGBA")
