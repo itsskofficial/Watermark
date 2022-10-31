26043f55da6d09ed05becc1064434325a8294440
SK <sarthakkarandikar03@gmail.com>
2022-10-31 06:00:39 +0000

    Oct 31, 2022, 11:30 AM

--- utils.py
+++ utils.py
@@ -4,11 +4,10 @@ from tkinter import messagebox
 
 
 def image_file():
-    file=filedialog.askopenfile(mode="rb",filetypes=[('JPG File', '*.jpg'),('PNG File','*./png')])
+    file=tkinter.filedialog.askopenfile(mode="rb",filetypes=[('JPG File', '*.jpg'),('PNG File','*./png')])
     if file is not None:
         img=file.read()
     else:
-        messagebox.showerror('Upload error','No such file found')
+        tkinter.messagebox.showerror('Upload error','No such file found')
 
 def watermark_image(img):
-    
