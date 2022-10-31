551bf0facff795bbd4549fac4791a049fb9f6d42
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:42:57 +0000

    Oct 31, 2022, 10:12 AM

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ canvas=tkinter.Canvas(screen,height=100,width=100,bg="black")
 canvas.place(x=200,y=50)
 label=tkinter.Label(screen,text="Upload your image below")
 label.place(x=185,y=200)
+
 screen.mainloop()
