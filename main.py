dd600a50149372b4e2305463a756d15e48819cfa
SK <sarthakkarandikar03@gmail.com>
2022-10-31 08:44:01 +0000

    Oct 31, 2022, 2:14 PM

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def image_file():
     if file is not None:
         img_bytes=file.read()
         stream = BytesIO(img_bytes)
+        img = Image.open(stream).convert("RGBA")
+        stream.close()
         print(img)
         watermark_page(img)
     else:
