3b0f73b73af5f9acfaa58efef1f1696cc6944e75
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:46:57 +0000

    Oct 31, 2022, 10:16 AM

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import tkinter
-from PIL import *
 
 screen=tkinter.Tk()
 screen.title("Watermark")
 screen.geometry("500x500")
 canvas=tkinter.Canvas(screen,height=100,width=100,bg="black")
 canvas.place(x=200,y=50)
-logo=im
+logo=tkinter.PhotoImage()
 label=tkinter.Label(screen,text="Upload your image below")
 label.place(x=185,y=200)
 
