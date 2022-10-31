36554fab3acdd7e88b0c0634543c608c152e81af
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:29:26 +0000

    Oct 31, 2022, 9:59 AM

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import tkinter
 screen=tkinter.Tk()
 screen.title("Watermark")
 screen.geometry("500x500")
-label=tkinter.Label(screen,text="Welcome to watermark")
+label=tkinter.Label(screen,text="Welcome")
 label.grid(row=0,column=0)
 screen.mainloop()
