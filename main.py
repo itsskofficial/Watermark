2ddfe31c9485a6d4ba8edcfa4435b0819beabdd5
SK <sarthakkarandikar03@gmail.com>
2022-10-31 05:54:51 +0000

    Oct 31, 2022, 11:24 AM

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ logo=Image.open("./logo.jpg")
 resized_logo=logo.resize((100,100),Image.ANTIALIAS)
 new_logo=ImageTk.PhotoImage(image=resized_logo)
 canvas.create_image(0, 0, image=new_logo, anchor=tkinter.NW)
-label = tkinter.Label(screen, text="Get your image watermarked", font=('Montserrat ))
+label = tkinter.Label(screen, text="Get your image watermarked", font=('Montserrat bold'))
 label.place(x=185, y=200)
-
 button=tkinter.Button(screen,text="Upload image",command=image_file)
 
 screen.mainloop()
