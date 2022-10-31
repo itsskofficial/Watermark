0084d722ac940544cad591785987d681af24915e
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:48:13 +0000

    Oct 31, 2022, 10:18 AM

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ screen.geometry("500x500")
 canvas=tkinter.Canvas(screen,height=100,width=100,bg="black")
 canvas.place(x=200,y=50)
 logo=tkinter.PhotoImage(file="logo.jpeg")
+canvas.create_image()
 label=tkinter.Label(screen,text="Upload your image below")
 label.place(x=185,y=200)
 
