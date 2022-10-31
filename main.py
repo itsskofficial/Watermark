bef9b32bfe929ebc284fce4845b9de6c7fcd2266
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:31:55 +0000

    Oct 31, 2022, 10:01 AM

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter
 screen=tkinter.Tk()
 screen.title("Watermark")
 screen.geometry("500x500")
-canvas=tkinter.Canvas(screen,)
+canvas=tkinter.Canvas(screen,height=)
 label=tkinter.Label(screen,text="Welcome")
 label.grid(row=0,column=1)
 screen.mainloop()
