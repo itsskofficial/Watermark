4e7f0dd5a36c9f3aea6dd37eef195bf5e0f59866
SK <sarthakkarandikar03@gmail.com>
2022-10-31 06:38:28 +0000

    Oct 31, 2022, 12:08 PM

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def watermark_page(img):
     canvas.destroy()
     label.destroy()
     button.destroy()
-    user_image=ImageTk.PhotoImage()
+    user_image=ImageTk.PhotoImage(img)
+    
 
 screen.mainloop()
