39e0e938579696ac8f957a30234035e4e68dfe50
SK <sarthakkarandikar03@gmail.com>
2022-10-31 05:52:21 +0000

    Oct 31, 2022, 11:22 AM

--- utils.py
+++ utils.py
@@ -4,4 +4,5 @@ from tkinter import filedialog
 
 def image_file():
     file=filedialog.askopenfile(mode="rb",filetypes=[('JPG File', '*.jpg'),('PNG File','*./png')])
-    if file
+    if file is not None:
+        
