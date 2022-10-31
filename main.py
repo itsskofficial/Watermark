0fa802ce7e9184f7e55325e7698abc26078b3cff
SK <sarthakkarandikar03@gmail.com>
2022-10-31 04:27:29 +0000

    Oct 31, 2022, 9:57 AM

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter
 
 screen=tkinter.Tk()
-screen.geometry("300x400")
+screen.geometry("500x400")
 label=tkinter.Label(screen,text="Hello")
 label.grid(row=0,column=0)
 screen.mainloop()
