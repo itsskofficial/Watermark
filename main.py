2099b41701a0546d42f19d4c83efd3d0813ea222
SK <sarthakkarandikar03@gmail.com>
2022-11-02 03:40:08 +0000

    Nov 2, 2022, 9:10 AM

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def watermark_image():
     base=img.convert("RGBA")
     text=Image.new("RGBA",base.size,(255,255,255,0))
     d=ImageDraw.Draw(text)
+    fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
     d.text((0,0),"SK",font="Arial",fill=(255,255,255,128))
     out=Image.alpha_composite(base,text)
     out.show()
