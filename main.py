8032a77a6265410325b6e857ffa0584959c06d6f
SK <sarthakkarandikar03@gmail.com>
2022-11-02 03:16:03 +0000

    Nov 2, 2022, 8:46 AM

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def watermark_page():
     resized_img=img.resize((300,300),Image.ANTIALIAS)
     new_img=ImageTk.PhotoImage(image=resized_img)
     print(new_img)
+    new_img.sav
     canvas2.create_image(0,0,image=new_img,anchor=tkinter.NW)
     button2=tkinter.Button(screen,text="Watermark image",command=watermark_image)
 
