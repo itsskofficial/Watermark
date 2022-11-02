0f430cc67b7fa71d20ffe44de07a573efe1d4c88
SK <sarthakkarandikar03@gmail.com>
2022-11-02 03:10:37 +0000

    Nov 2, 2022, 8:40 AM

--- utils.py
+++ utils.py
@@ -11,10 +11,4 @@ def image_file():
         tkinter.messagebox.showerror('Upload error','No such file found')
 
 
-def watermark_image(img):
-    base=img.convert("RGBA")
-    text=Image.new("RGBA",base.size,(255,255,255,0))
-    d=ImageDraw.Draw(text)
-    d.text((0,0),"SK",font="Arial",fill=(255,255,255,128))
-    out=Image.alpha_composite(base,text)
-    return out
+
