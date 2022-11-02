cb8ec46b76a97df4cb2ab2ea7ff58260e57c4e7b
SK <sarthakkarandikar03@gmail.com>
2022-11-02 03:37:41 +0000

    Nov 2, 2022, 9:07 AM

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def watermark_page():
     print(new_img)
     resized_img.save(fp='resizedimage.jpg')
     screen.new_img=new_img
-    canvas2.create_image(0,0,image=new_img)
+    canvas2.create_image((0,0),image=new_img)
     button2=tkinter.Button(screen,text="Watermark image",command=watermark_image)
     button2.place(x=200,y=400)
 
